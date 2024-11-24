65a48bfc646c5922b5bd70adee4401b3fa0bfd69
Ana <Ana@DESKTOP-A2VV5FI>
2024-11-24 20:54:51 +0200

    Add project files.

---
+++ PerijocAnaMariaLab7/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PerijocAnaMariaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
